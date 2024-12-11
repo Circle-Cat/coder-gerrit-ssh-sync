7af4a188faf456cced4ed041d06e8bb70840465a
Jiaxuan Zhao <jzhao@u.circlecat.org>
2024-12-11 20:16:58 +0000

    [OSS-15] Refactor coderInstance and coderToken to non-global variables.
    This change is to follow the best practice to avoid using global variables if can achieve by using local variables.
    
    Change-Id: I5f6d5f64c39322b9274b6cb664766e9c8ce5e6e8

---
+++ pkg/coderclient/client.go
@@ -0,0 +1,64 @@
+package coderclient
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+// CoderClient is a client for interacting with the Coder API.
+type CoderClient struct {
+	// url is the base URL of Coder API
+	url string
+
+	// token is the authentication token for API requests
+	token string
+
+	// client is the HTTP client used to make requests to Coder API
+	client *http.Client
+}
+
+// CoderBuildInfoResponse includes the version of the Coder system.
+type CoderBuildInfoResponse struct {
+	Version string `json:"version"`
+}
+
+// NewCoderClient returns a pointer coderClient (reference).
+func NewCoderClient(url string, token string) *CoderClient {
+	return &CoderClient{
+		url:    url,
+		token:  token,
+		client: http.DefaultClient, // Assign http global client reference to client
+	}
+}
+
+// Get sends an HTTP GET request to the specified path using the coderClient.
+// It decodes the JSON response into the target variable.
+func (c *CoderClient) Get(ctx context.Context, path string, target any) error {
+
+	fullURL, err := url.JoinPath(c.url, path)
+	if err != nil {
+		return fmt.Errorf("failed to join URL path: %w", err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fullURL, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Coder-Session-Token", c.token)
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Coder HTTP status: %s", resp.Status)
+	}
+
+	return json.NewDecoder(resp.Body).Decode(target)
+}
