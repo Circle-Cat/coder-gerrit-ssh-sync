f060c84a63c2a2e4d8b0141dc988417b1e74cdbd
Jiaxuan Zhao <jzhao@u.circlecat.org>
2024-12-14 08:27:41 +0000

    test2
    Change-Id: Ib0a40ab1a28706fcee984becdba027fc7cc5c5ae

--- cmd/coder-gerrit-ssh-sync/main.go
+++ cmd/coder-gerrit-ssh-sync/main.go
@@ -19,15 +19,4 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strings"
-
-	"github.com/andygrunwald/go-gerrit"
-	"github.com/jingyuanliang/coder-gerrit-ssh-sync/pkg/version"
-	flag "github.com/spf13/pflag"
 )
